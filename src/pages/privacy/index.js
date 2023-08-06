ddcd48ae7660bfdbc278f6c0fc490650b84baf5c
Junsuk Park <park64kr63@gmail.com>
2023-08-06 21:29:12 +0900

    privacy

---
+++ src/pages/privacy/index.js
@@ -0,0 +1,30 @@
+import {Head} from "next/document";
+
+export default function Privacy() {
+    return (
+        <>
+            <h1>Privacy Policy of CalendarBlock</h1>
+                <br />
+            <h2>1. Introduction</h2>
+            <p>At CalendarBlock, we value your privacy. This Privacy Policy outlines how we handle and protect your personal information when you use our time management application ("App").</p>
+
+            <h2>2. Information We Collect</h2>
+            <p>The CalendarBlock App is designed to respect your privacy. We do not collect or store any personal data. All the information you input into the App remains solely on your device and is not transmitted or stored outside your device.</p>
+
+            <h2>3. How We Use the Information</h2>
+            <p>As we do not collect any personal data, we do not use any such data for any purpose.</p>
+
+            <h2>4. Information We Share</h2>
+            <p>We do not collect, and therefore do not share, any personal information with any companies, organizations, or individuals outside of CalendarBlock.</p>
+
+            <h2>5. Security</h2>
+            <p>While we don’t collect personal information, we prioritize the security of our users. We encourage you to keep your device secure to protect the information that the App stores on your device.</p>
+
+            <h2>6. Changes to This Policy</h2>
+            <p>We may revise this Privacy Policy from time to time, and will post the most current version on our website.</p>
+
+            <h2>7. Contact Us</h2>
+            <p>If you have any questions about this Privacy Policy, please contact us at: [Your Company's contact information].</p>
+        </>
+    )
+}
